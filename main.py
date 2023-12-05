ae6e43288f91e39dbd372923aceadba429d10115
Johan Trieloff <johan@trieloff.net>
2023-12-05 20:41:47 +0100

    Update main.py to track changes in JavaScript functions

--- main.py
+++ main.py
@@ -4,10 +4,11 @@
 - Should we use save the first function or the first function after the first merge commit?
 - Should we consider only incrementing the value if 'fix' or 'bug' is in the commit message?
 """
-
 import requests
 import time
 import os
+import re
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -29,9 +30,9 @@ def fetch_all_commits(repo_api_url, headers):
             raise Exception(f'Failed to fetch commits: {response.status_code}')
     return commits
 
-def analyze_commit_for_js_files(commit_url, headers):
-    """ Analyze a specific commit for changes in JavaScript files """
-    js_files_changed = []
+def analyze_commit_for_js_changes(commit_url, headers):
+    """ Analyze a specific commit for changes in JavaScript functions """
+    function_changes = {}
     attempt = 0
     max_attempts = 5
 
@@ -42,8 +43,11 @@ def analyze_commit_for_js_files(commit_url, headers):
                 commit_data = response.json()
                 for file in commit_data.get('files', []):
                     if file['filename'].endswith('.js'):
-                        js_files_changed.append(file['filename'])
-                return js_files_changed
+                        patch = file.get('patch', '')
+                        functions_changed = extract_functions_from_patch(patch)
+                        for func in functions_changed:
+                            function_changes[func] = function_changes.get(func, 0) + 1
+                return function_changes
             else:
                 raise Exception(f'Failed to fetch commit details: {response.status_code}')
         except requests.exceptions.RequestException as e:
@@ -53,6 +57,15 @@ def analyze_commit_for_js_files(commit_url, headers):
 
     raise Exception("Max retries reached")
 
+def extract_functions_from_patch(patch):
+    """ Extract function names from a commit patch """
+    function_names = set()
+    pattern = r'\+function (\w+)\s*\('
+    matches = re.findall(pattern, patch)
+    for match in matches:
+        function_names.add(match)
+    return function_names
+
 def main():
     token = os.getenv('GITHUB_TOKEN')
     headers = {'Authorization': f'token {token}'} if token else {}
@@ -61,10 +74,21 @@ def main():
 
     try:
         commits = fetch_all_commits(repo_api_url, headers)
+        overall_function_changes = {}
+
         for commit in commits:
             commit_url = commit['url']
-            js_files_changed = analyze_commit_for_js_files(commit_url, headers)
-            print(f"Commit {commit['sha']} changed JS files: {js_files_changed}")
+            function_changes = analyze_commit_for_js_changes(commit_url, headers)
+            for func, count in function_changes.items():
+                overall_function_changes[func] = overall_function_changes.get(func, 0) + count
+                print(f"Function {func} changed {count} times")
+
+        # Convert the result to JSON and write to a file
+        with open('function_changes.json', 'w') as file:
+            json.dump(overall_function_changes, file, indent=4)
+
+        print("Function change data written to function_changes.json")
+
     except Exception as e:
         print(f"Error: {e}")
 
