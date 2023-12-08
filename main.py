eec9bb9f3ae2e8ca96365af5bc475423b9705fe7
Johan Trieloff <johan@trieloff.net>
2023-12-08 16:32:51 +0100

    print results in json

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ How to improve this Code:
 3. Filter comment and whitespace changes
 4. Start increasing the number of commits only after the first merge commit
 5. Use a more sophisticated hashing algorithm
-6. Use a more sophisticated regular expression to match functions
+6. Use a more sophisticated regular expression to match function declarations and bodies
 7. Add more tests
+8. Write the full function along with the commit into a json file
 """
 import subprocess
 import re
+import json
 from hashlib import sha256
 
 def get_js_commits(repo_path):
@@ -33,7 +35,7 @@ def hash_function_body(function_body):
 def analyze_commits(repo_path, commits):
     function_changes = {}
 
-    for commit in commits:
+    for commit in reversed(commits):  # Reverse the commit list to start from the earliest commit
         diff = get_commit_diff(repo_path, commit)
         functions = extract_functions(diff)
 
@@ -43,8 +45,13 @@ def analyze_commits(repo_path, commits):
             function_hash = hash_function_body(full_function)
 
             if function_name not in function_changes:
-                function_changes[function_name] = {'count': 0, 'hashes': set()}
-            
+                # Store the first version of the function and initialize the count and hash set
+                function_changes[function_name] = {
+                    'count': 0,
+                    'hashes': set(),
+                    'first_version': full_function  # Store the first version of the function
+                }
+
             if function_hash not in function_changes[function_name]['hashes']:
                 function_changes[function_name]['count'] += 1
                 function_changes[function_name]['hashes'].add(function_hash)
@@ -56,8 +63,17 @@ def main():
     commits = get_js_commits(repo_path)
     function_changes = analyze_commits(repo_path, commits)
 
+    # Prepare data for JSON output
+    output_data = {function_name: {'count': data['count'], 'first_version': data['first_version']} 
+                   for function_name, data in function_changes.items()}
+
+    # Write to JSON file
+    with open('./outputData/function_changes.json', 'w') as json_file:
+        json.dump(output_data, json_file, indent=4)
+
     for function_name, data in function_changes.items():
         print(f"Function '{function_name}' changed {data['count']} times")
 
 if __name__ == "__main__":
     main()
+
