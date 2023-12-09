91b61cd3d44160bbe696a9d0087e285a5aa4ed87
Johan Trieloff <johan@trieloff.net>
2023-12-09 15:45:29 +0100

    create Embeddings for all of json files

--- Embeddings/createEmbeddings.py
+++ Embeddings/createEmbeddings.py
@@ -8,28 +8,34 @@ import dotenv
 dotenv.load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Define the input text for which you want to get the embedding
-input_text = "function testingTheChange() {\n+  console.log('This the original function');\n+}"
+# Load the JSON data from the file
+json_file_path = '../outputData/function_changes.json'
+with open(json_file_path, 'r') as file:
+    json_data = json.load(file)
 
 # Specify the embedding model
 model = "text-embedding-ada-002"
 
-# Create the data for the POST request
-data = {
-    "input": input_text,
-    "model": model
-}
+# Process each function in the JSON data
+for function_name, function_data in json_data.items():
+    input_text = function_data['first_version']
 
-# Make the API request
-response = requests.post(
-    'https://api.openai.com/v1/embeddings',
-    headers={
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {openai.api_key}'
-    },
-    data=json.dumps(data)
-)
+    # Create the data for the POST request
+    data = {
+        "input": input_text,
+        "model": model
+    }
 
-# Extract and print the embedding from the response
-embedding = response.json()['data'][0]['embedding']
-print(embedding)
+    # Make the API request
+    response = requests.post(
+        'https://api.openai.com/v1/embeddings',
+        headers={
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {openai.api_key}'
+        },
+        data=json.dumps(data)
+    )
+
+    # Extract and print the embedding from the response
+    embedding = response.json()['data'][0]['embedding']
+    print(f"Embedding for {function_name}:", embedding)
