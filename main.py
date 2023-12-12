d1542bdb67106c850e372cc3fafa48e84b398793
Johan_Trieloff <johan@trieloff.net>
2023-12-12 18:34:45 +0100

    tracks the values but not optimal

--- main.py
+++ main.py
@@ -10,30 +10,21 @@ merge_commits = [commit for commit in repo.iter_commits('main') if commit.parent
 merge_commits.reverse() # Reverse the list to get the oldest merge commit first 
 
 def extract_js_functions(diff):
-    # Regular expression to match JavaScript function declarations
     pattern = re.compile(r'function\s+(\w+)\s*\((.*?)\)\s*\{([\s\S]*?)\}', re.MULTILINE)
     return pattern.findall(diff)
 
 def get_full_function_at_commit(repo, commit_hash, function_name, file_path):
-    # Get the commit object
     commit = repo.commit(commit_hash)
+    blob = commit.tree / file_path
+    file_content = blob.data_stream.read().decode('utf-8')
 
-    # Find the file in the commit tree
-    for blob in commit.tree.traverse():
-        if blob.path == file_path:
-            # Read the file content
-            file_content = blob.data_stream.read().decode('utf-8')
+    pattern = re.compile(r'function\s+' + re.escape(function_name) + r'\s*\((.*?)\)\s*\{([\s\S]*?)\}', re.MULTILINE)
+    match = pattern.search(file_content)
 
-            # Regular expression to match the specific function
-            pattern = re.compile(r'function\s+' + re.escape(function_name) + r'\s*\((.*?)\)\s*\{([\s\S]*?)\}', re.MULTILINE)
-            match = pattern.search(file_content)
+    if match:
+        full_function = f"function {function_name}({match.group(1)}) {{{match.group(2)}}}"
+        return full_function
 
-            if match:
-                # Construct the full function declaration
-                full_function = f"function {function_name}({match.group(1)}) {{{match.group(2)}}}"
-                return full_function
-
-    print(f"Function '{function_name}' not found in commit '{commit_hash}'")
     return None
 
 functions = {}
@@ -42,19 +33,28 @@ for commit in merge_commits:
     parent_commit = commit.parents[0]
     diffs = commit.diff(parent_commit, create_patch=True)
 
-    print(f"Commit: {commit.hexsha}")
     for diff in diffs:
-        print(f"File: {diff.a_path} (Change Type: {diff.change_type})")
         diff_content = diff.diff.decode('utf-8')
         for func_name, _, _ in extract_js_functions(diff_content):
-            if func_name not in functions:
-                full_function = get_full_function_at_commit(repo, commit.hexsha, func_name, diff.a_path)
-                if full_function:
-                    functions[func_name] = {'function': full_function, 'commit': commit.hexsha}
-                    print(f"Function '{func_name}' captured from merge commit.")
+            full_function = get_full_function_at_commit(repo, commit.hexsha, func_name, diff.a_path)
+            if full_function:
+                if func_name not in functions:
+                    functions[func_name] = {
+                        'function': full_function,
+                        'commit': commit.hexsha,
+                        'change_count': 0,
+                        'latest_content': full_function
+                    }
+                elif functions[func_name]['latest_content'] != full_function:
+                    functions[func_name]['change_count'] += 1
+                    functions[func_name]['latest_content'] = full_function
+                    print(f"Function {func_name} changed at commit {commit.hexsha}")
 
-    print("\n")
-
-# Save the functions into a file called 'function_changes.json' in ./outputData
+# Save the functions and their change counts into a file
 with open('./outputData/function_changes.json', 'w') as fp:
     json.dump(functions, fp, indent=4)
+
+# Current issues
+# - Functions with the same name are treated as the same function
+# - Functions that are renamed are treated as different functions
+# - Does not keep track of changes that are not merge commits (When a function was changed on main or multiple commits were merged into main)
