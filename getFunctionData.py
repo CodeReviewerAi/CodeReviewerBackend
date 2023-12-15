e8f24f96863a291dc9046d99bae266b047ac1e75
Johan Trieloff <johan@trieloff.net>
2023-12-15 23:23:51 +0100

    Update .gitignore to include all JSON files and remove unnecessary files

---
+++ getFunctionData.py
@@ -0,0 +1,72 @@
+import git
+import json
+import re
+
+def get_function_data():
+    # Path to your repository
+    repo_path = '../inputData/testRepo2'
+    repo = git.Repo(repo_path)
+
+    merge_commits = [commit for commit in repo.iter_commits('main') if commit.parents and len(commit.parents) > 1]
+    merge_commits.reverse()  # Reverse the list to get the oldest merge commit first
+
+    def get_func_name(diff):
+        pattern = re.compile(r'function\s+(\w+)\s*\((.*?)\)\s*\{([\s\S]*?)\}', re.MULTILINE)
+        return pattern.findall(diff)
+
+    def get_full_function_at_commit(repo, commit_hash, function_name, file_path): 
+        commit = repo.commit(commit_hash)
+        blob = commit.tree / file_path
+        file_content = blob.data_stream.read().decode('utf-8')
+
+        pattern = re.compile(r'function\s+' + re.escape(function_name) + r'\s*\((.*?)\)\s*\{([\s\S]*?)\}', re.MULTILINE)
+        match = pattern.search(file_content)
+
+        if match:
+            full_function = f"function {function_name}({match.group(1)}) {{{match.group(2)}}}"
+            return full_function
+
+        return None
+
+    functions = {}
+
+    for commit in merge_commits:
+        parent_commit = commit.parents[0]
+        diffs = commit.diff(parent_commit, create_patch=True)
+
+        for diff in diffs:
+            diff_content = diff.diff.decode('utf-8')
+            for func_name, _, _ in get_func_name(diff_content):
+                full_function = get_full_function_at_commit(repo, commit.hexsha, func_name, diff.a_path)
+                if full_function:
+                    if func_name not in functions:
+                        functions[func_name] = {
+                            'merged_function': full_function,
+                            'commit': commit.hexsha,
+                            'changes_after_merge': 0,
+                            'latest_function': full_function,
+                            'time_first_merged': commit.authored_datetime,
+                            'file_path': diff.a_path  # Store file path here
+                        }
+
+    for func_name, func_info in functions.items():
+        for commit in repo.iter_commits('main', reverse=True):  # Iterate from the oldest to newest
+            if commit.authored_datetime > func_info['time_first_merged']:
+                try:
+                    blob = commit.tree / func_info['file_path']
+                    file_content = blob.data_stream.read().decode('utf-8')
+                    new_content = get_full_function_at_commit(repo, commit.hexsha, func_name, func_info['file_path'])
+                    if new_content and new_content.strip() != func_info['latest_function'].strip():
+                        func_info['changes_after_merge'] += 1
+                        func_info['latest_function'] = new_content
+                        print(f"Function '{func_name}' changed at commit {commit.hexsha}")
+                except KeyError:
+                    continue
+
+    # Convert datetime objects to string before saving
+    for func in functions.values():
+        func['time_first_merged'] = func['time_first_merged'].isoformat()
+
+    # Save the functions and their change counts into a file
+    with open('./outputData/function_changes.json', 'w') as fp:
+        json.dump(functions, fp, indent=4)
--- userInput/processUserInput.py
+++ userInput/processUserInput.py
@@ -6,64 +6,65 @@ import dotenv
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
 
-dotenv.load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+def process_user_input():
+    dotenv.load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def embedUserInput():
-    Json_path = 'userInput/inputFunction.json'
-    with open(Json_path, 'r') as file:
-        json_data = json.load(file)
+    def embedUserInput():
+        Json_path = 'userInput/inputFunction.json'
+        with open(Json_path, 'r') as file:
+            json_data = json.load(file)
 
-    model = "text-embedding-ada-002"
-    input_text = json_data['function']
+        model = "text-embedding-ada-002"
+        input_text = json_data['function']
 
-    data = {
-        "input": input_text,
-        "model": model
-    }
-    # standard Opeanai call
-    response = requests.post(
-        'https://api.openai.com/v1/embeddings',
-        headers={
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {openai.api_key}'
-        },
-        data=json.dumps(data)
-    )
-    embedding = response.json()['data'][0]['embedding']
-    return embedding
+        data = {
+            "input": input_text,
+            "model": model
+        }
+        # standard Opeanai call
+        response = requests.post(
+            'https://api.openai.com/v1/embeddings',
+            headers={
+                'Content-Type': 'application/json',
+                'Authorization': f'Bearer {openai.api_key}'
+            },
+            data=json.dumps(data)
+        )
+        embedding = response.json()['data'][0]['embedding']
+        return embedding
 
-def performKNNSearch(embedding, k=5):
-    # Initialize the Qdrant client
-    client = QdrantClient(host='localhost', port=6333)
+    def performKNNSearch(embedding, k=5):
+        # Initialize the Qdrant client
+        client = QdrantClient(host='localhost', port=6333)
 
-    # Perform the k-NN search
-    search_results = client.search(
-        collection_name="functions",
-        query_vector=embedding,
-        search_params=models.SearchParams(hnsw_ef=128, exact=False), # No idea what this does
-        limit=k
-    )
+        # Perform the k-NN search
+        search_results = client.search(
+            collection_name="functions",
+            query_vector=embedding,
+            search_params=models.SearchParams(hnsw_ef=128, exact=False), # No idea what this does
+            limit=k
+        )
 
 
-   # Check if 'result' is a valid key in the search results
-    if 'result' in search_results:
-        results = search_results['result']
-    else:
-        # If 'result' is not a key, assume search_results is a list of results
-        results = search_results
+    # Check if 'result' is a valid key in the search results
+        if 'result' in search_results:
+            results = search_results['result']
+        else:
+            # If 'result' is not a key, assume search_results is a list of results
+            results = search_results
 
-    # Extract the payloads and compute the average
-    total_score = 0
-    for result in results:
-        # Access the payload and score using the appropriate method or property
-        payload = result.payload
-        score = payload['score'] if 'score' in payload else 0
-        total_score += score
+        # Extract the payloads and compute the average
+        total_score = 0
+        for result in results:
+            # Access the payload and score using the appropriate method or property
+            payload = result.payload
+            score = payload['score'] if 'score' in payload else 0
+            total_score += score
 
-    average_score = total_score / k 
+        average_score = total_score / k 
 
-    return average_score
+        return average_score
 
-embed = embedUserInput()
-print('Average Score:', performKNNSearch(embed))
+    embed = embedUserInput()
+    print('Average Score:', performKNNSearch(embed))
