2b96ad5a7634d81328270ba8cbe7fb0b6f863b85
Johan Trieloff <johan@trieloff.net>
2023-12-19 15:47:18 +0100

    added all tests

--- getFunctionData.py
+++ getFunctionData.py
@@ -41,14 +41,14 @@ def get_function_data():
     for commit in merge_commits:
         parent_commit = commit.parents[0]
         diffs = commit.diff(parent_commit, create_patch=True)
-        print(f"Merge commit {commit.hexsha} with message '{commit.message}'")
+        # print(f"Merge commit {commit.hexsha} with message '{commit.message}'")
 
         for diff in diffs:
             diff_content = diff.diff.decode('utf-8')
-            print(f"Diff for file '{diff.a_path}':\n{diff_content}")
+            #print(f"Diff for file '{diff.a_path}':\n{diff_content}")
             for func_name, _ in get_func_name(diff_content):
                 full_function = get_full_function_at_commit(repo, commit.hexsha, func_name, diff.a_path)
-                print(f"Found function '{func_name}' in file '{diff.a_path}'")
+                #print(f"Found function '{func_name}' in file '{diff.a_path}'")
                 #print(f"Full function: {full_function}")
                 if full_function:
                     func_key = f"{diff.a_path}::{func_name}"
@@ -75,7 +75,7 @@ def get_function_data():
                     if new_content and new_content.strip() != func_info['latest_function'].strip():
                         func_info['changes_after_merge'] += 1
                         func_info['latest_function'] = new_content
-                        print(f"Function '{func_info['function_name']}' changed at commit {commit.hexsha}")
+                        #print(f"Function '{func_info['function_name']}' changed at commit {commit.hexsha}")
                 except KeyError:
                     continue
 
@@ -87,7 +87,7 @@ def get_function_data():
     for func_key, func_info in functions.items():
         if max_changes != min_changes:
             normalized_score = 2 * ((func_info['changes_after_merge'] - min_changes) / (max_changes - min_changes)) - 1
-            print(f"Function '{func_info['function_name']}' has a normalized score of {normalized_score}")
+            #print(f"Function '{func_info['function_name']}' has a normalized score of {normalized_score}")
         else:
             normalized_score = 0
         func_info['score'] = normalized_score
--- test_get_function_data.py
+++ test_get_function_data.py
@@ -79,6 +79,21 @@ class TestFunctionData(unittest.TestCase):
         self.assertEqual(self.function_data[expected_key]['merged_function'].strip(), expected_function['merged_function'].strip())
         self.assertEqual(self.function_data[expected_key]['changes_after_merge'], expected_function['changes_after_merge'])
         
+    def test_changedAfterMergeWithMerge(self):
+        # Define the expected function key and content
+        expected_key = 'blocks/tests.js::changedAfterMergeWithMerge'
+        expected_function = {
+            'function_name': 'changedAfterMergeWithMerge',
+            'merged_function': 'function changedAfterMergeWithMerge() {\n    this is the first change\n    this is the second change\n}',
+            'changes_after_merge': 2
+        }
+
+        # Check if the function data contains the expected function with the correct data
+        self.assertIn(expected_key, self.function_data)
+        self.assertEqual(self.function_data[expected_key]['function_name'], expected_function['function_name'])
+        self.assertEqual(self.function_data[expected_key]['merged_function'].strip(), expected_function['merged_function'].strip())
+        self.assertEqual(self.function_data[expected_key]['changes_after_merge'], expected_function['changes_after_merge'])
+        
 class CustomTestRunner(unittest.TextTestRunner):
     def run(self, test):
         result = super(CustomTestRunner, self).run(test)
