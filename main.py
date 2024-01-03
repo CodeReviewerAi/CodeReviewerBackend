d09a2a55e0b65b2eba9dde1ea82f68c2503959f7
Johan_Trieloff <johan@trieloff.net>
2024-01-03 22:07:46 +0100

    Refactor embedding function and add support for different repositories

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ import getFunctionData
 import createEmbeddings
 from userInput import processUserInput
 
-# Run the main function from getFunctionData
-# pass this variable if you want to run another repo than testRepo: 
-# repo_path='../inputData/elixirsolutions'
-getFunctionData.get_function_data()
+def main(repo_path='../inputData/testRepo'):
+    # Run the main function from getFunctionData
+    getFunctionData.get_function_data(repo_path)
 
-# Run the main function from createEmbeddings
-createEmbeddings.embed_sample_functions()
+    # Run the main function from createEmbeddings
+    createEmbeddings.embed_sample_functions(repo_path)
 
-# Run the main function from processUserInput
-processUserInput.process_user_input()
+    # Run the main function from processUserInput
+    processUserInput.process_user_input()
 
-# Create a test suite
-suite = unittest.TestLoader().loadTestsFromModule(test_get_function_data)
+    # Create a test suite
+    suite = unittest.TestLoader().loadTestsFromModule(test_get_function_data)
 
-# Run the tests with CustomTestRunner
-test_get_function_data.CustomTestRunner().run(suite)
+    # Run the tests with CustomTestRunner
+    test_get_function_data.CustomTestRunner().run(suite)
+
+if __name__ == '__main__':
+     # pass this variable if you want to run another repo than testRepo: 
+     # repo_path='../inputData/elixirsolutions'
+    main()
